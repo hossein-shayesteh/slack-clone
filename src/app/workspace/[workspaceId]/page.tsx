b4e6a07a8f77da98d8ddefc1b68fd7e8febea1cb
Hossein Shayesteh <hosseinxshayesteh@gmail.com>
2024-10-13 14:35:26 +0330

    feat: Add endpoint to fetch workspace by ID and hook for data retrieval
    - Created an endpoint to retrieve workspace details by ID from the database
    - Developed a hook to connect to the endpoint and fetch workspace data

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,10 +1,13 @@
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+"use client";
 
-const WorkspaceIdPage = ({ params: { workspaceId } }: WorkspaceIdPageProps) => {
+import { useGetWorkspace } from "@/src/features/workspace/api/use-get-workspace";
+import { useWorkspaceId } from "@/src/features/workspace/hooks/use-workspace-id";
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace(workspaceId);
+
+  console.log(data);
   return <div>ID: {workspaceId}</div>;
 };
 export default WorkspaceIdPage;
